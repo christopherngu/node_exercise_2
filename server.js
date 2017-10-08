fb71fa1c5b16b57441daddcb12ed3c159db305f0
Christopher Nguyen <chrihn15@student.uia.no>
2017-10-09 01:22:25 +0200

    node2

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+function rnGreeting() {
+    
+    var arr = ["hello", "ciao", "howdy", "greetings", "salut", "hola", "Hey"]
+    
+    var greeting = arr[Math.floor(Math.random() * 4)];
+    
+    
+    return greeting;
+}
+
+app.get('/gr/:name', function(req, res) {
+  res.send(rnGreeting() + " " + req.params.name)
+})
+
+app.listen(8080, function(){
+    console.log("The server started on port 8080");
+})
+
